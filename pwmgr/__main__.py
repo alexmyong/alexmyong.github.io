00a951c269053e58139a9884a5919a60a2846fd4
Alex Yong <alex@alexyong.com>
2025-01-02 12:56:02 +0000

    Purge

--- pwmgr/__main__.py
+++
@@ -1,9 +0,0 @@
-def file_to_list(filename):
-    with open(filename,'r') as file:
-        numbers = [line.strip() for line in file.readlines()]
-    return license
-
-filename = 'numbers.txt'
-numbers = file_to_list(filename)
-
-print(numbers)
